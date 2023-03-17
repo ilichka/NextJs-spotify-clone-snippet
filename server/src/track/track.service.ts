6ba22b4d09437d12b7665d9dfd6e0aaffa62e263
Ilya Parkhomenko <apapa2033@mail.ru>
2023-03-17 15:13:11 +0300

    feat(): init server side

---
+++ server/src/track/track.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {Track, TrackDocument} from "./schemas/track.schema";
+import {Comment, CommentDocument} from "./schemas/comment.schema";
+
+@Injectable()
+export class TrackService {
+
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,) {}
+
+    async create() {
+
+    }
+
+    async getAll() {
+
+    }
+
+    async getOne() {
+
+    }
+
+    async delete() {
+
+    }
+}
